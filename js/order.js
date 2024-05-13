f3769a7e538d554e9de4e0e654927249cd5e49ee
yjlee <gbsb227@gmail.com>
2024-05-13 20:21:01 +0900

    결제기능 연동 결제기능 연동과 내용 수정

--- js/order.js
+++ js/order.js
@@ -99,8 +99,67 @@ minusSuperStarButton.addEventListener("click", () => {
 
 const orderButton = document.querySelector(".order-button");
 
-orderButton.addEventListener("click", () => {
-  alert(
-    `슈퍼버섯 ${states.superMushroomState}개, 1up버섯 ${states.upMushroomState}개, 슈퍼스타 ${states.superStarState}개 주문`
-  );
+////////
+
+// import Bootpay from "@bootpay/client-js";
+// const bootpayCon = bootpay.Bootpay;
+// console.log(bootpayCon);
+
+orderButton.addEventListener("click", async () => {
+  if (authData.data.user === null) {
+    alert("로그인 후 이용하세요");
+    return;
+  }
+
+  // alert(
+  //   `슈퍼버섯 ${states.superMushroomState}개, 1up버섯 ${states.upMushroomState}개, 슈퍼스타 ${states.superStarState}개 주문`
+  // );
+  try {
+    const response = await Bootpay.requestPayment({
+      application_id: "59a4d323396fa607cbe75de4",
+      price: (superMushroomState + upMushroomState + superStarState) * 100,
+      order_name: "테스트결제",
+      order_id: "TEST_ORDER_ID",
+      pg: "다날",
+      method: "카드",
+      tax_free: 0,
+      user: {
+        id: authData.data.user.email,
+        username: authData.data.user.email,
+        phone: "01000000000",
+        email: authData.data.user.email,
+      },
+      items: [
+        {
+          id: "supermushroom",
+          name: "슈퍼버섯",
+          qty: superMushroomState,
+          price: 100,
+        },
+        {
+          id: "upmushroom",
+          name: "1up버섯",
+          qty: upMushroomState,
+          price: 100,
+        },
+        {
+          id: "superstar",
+          name: "슈퍼스타",
+          qty: superStarState,
+          price: 100,
+        },
+      ],
+      extra: {
+        open_type: "iframe",
+        card_quota: "0,2,3",
+        escrow: false,
+      },
+    });
+  } catch (err) {
+    if (err.error_code === "RC_PROCESS_CANCELLED") {
+      alert("결제가 취소되었습니다.");
+    } else {
+      alert("각 물건은 1개 이상 구매해야 합니다.");
+    }
+  }
 });
